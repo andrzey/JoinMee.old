16422f6d3f97e0fbf54b81f00484709704bbdb81
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2017-05-29 14:47:21 +0200

    added handlers for show and dismiss createModal

--- src/components/FirstTabScreen.js
+++ src/components/FirstTabScreen.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Text } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { Text, AlertIOS } from 'react-native';
+
+import { iconsMap } from '../utils/app-icons';
 
 class FirstTabScreen extends Component {
+
+
   constructor(props) {
     super(props)
+    this.props.navigator.setButtons({
+      rightButtons: [
+        {
+          id: 'add',
+          icon: iconsMap['ios-add'],
+        }
+      ]
+    })
+    this.props.navigator.setOnNavigatorEvent(this._onNavigatorEvent.bind(this));
+  }
+
+  _onNavigatorEvent(event) {
+    if (event.type == 'NavBarButtonPress') {
+      if (event.id == 'add') {
+        this.props.navigator.showModal({
+          screen: "example.CreateModal",
+          title: "Modal",
+          passProps: {},
+          navigatorButtons: {
+            rightButtons: [{
+              id: 'save',
+              title: 'Save',
+              buttonFontSize: 14,
+              buttonFontWeight: '600'
+            }],
+            leftButtons: [{
+              id: 'cancel',
+              title: 'Cancel',
+              buttonFontSize: 14,
+              buttonFontWeight: '600'
+            }]
+          },
+          animationType: 'slide-up'
+        });
+      }
+    }
   }
 
   render() {
