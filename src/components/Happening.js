995d40c7a25e89625b80675d6abefacfa6935377
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2017-05-30 09:22:09 +0200

    added a happening screen

---
+++ src/components/Happening.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native'
+
+class Happening extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Text>
+                Her er vi!
+            </Text>
+        );
+    }
+}
+
+export default Happening;
--- src/screens.js
+++ src/screens.js
@@ -3,9 +3,11 @@ import { Navigation } from 'react-native-navigation';
 import FirstTabScreen from './components/FirstTabScreen';
 import SecondTabScreen from './components/SecondTabScreen';
 import CreateModal from './components/CreateModal';
+import Happening from './components/Happening';
 
 export function registerScreens(store, Provider) {
   Navigation.registerComponent('example.FirstTabScreen', () => FirstTabScreen, store, Provider);
   Navigation.registerComponent('example.SecondTabScreen', () => SecondTabScreen, store, Provider);
   Navigation.registerComponent('example.CreateModal', () => CreateModal, store, Provider);
+  Navigation.registerComponent('example.Happening', () => Happening, store, Provider);
 }
