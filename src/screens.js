2f811bf23bd15659ed6e9dd969b1f9088ae59e5d
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2017-06-06 13:01:48 +0200

    added login

--- src/screens.js
+++ src/screens.js
@@ -4,10 +4,12 @@ import FirstTabScreen from './components/FirstTabScreen';
 import SecondTabScreen from './components/SecondTabScreen';
 import CreateModal from './components/CreateModal';
 import Happening from './components/Happening';
+import Login from './components/Login';
 
 export function registerScreens(store, Provider) {
   Navigation.registerComponent('example.FirstTabScreen', () => FirstTabScreen, store, Provider);
   Navigation.registerComponent('example.SecondTabScreen', () => SecondTabScreen, store, Provider);
   Navigation.registerComponent('example.CreateModal', () => CreateModal, store, Provider);
   Navigation.registerComponent('example.Happening', () => Happening, store, Provider);
+  Navigation.registerComponent('example.Login', () => Login, store, Provider);
 }
