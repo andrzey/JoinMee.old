7554da9b5fee1db29e11c6b6aa725c1746dda9e8
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2017-06-09 15:23:21 +0200

    changed android project to handle facebook sdk

--- android/app/src/main/java/com/joinmee/MainActivity.java
+++ android/app/src/main/java/com/joinmee/MainActivity.java
@@ -1,8 +1,14 @@
 package com.joinmee;
+import android.content.Intent;
 
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
 
 }
--- android/app/src/main/java/com/joinmee/MainApplication.java
+++ android/app/src/main/java/com/joinmee/MainApplication.java
@@ -11,24 +11,51 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.facebook.react.ReactPackage;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
+import android.support.annotation.NonNull;
+import com.reactnativenavigation.controllers.ActivityCallbacks;
+import android.content.Intent;
+
 
 public class MainApplication extends NavigationApplication {
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
-    public boolean isDebug() {
-     // Make sure you are using BuildConfig from your own application
-      return BuildConfig.DEBUG;
-    }
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new VectorIconsPackage()
-      );
-    }
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
 
-    @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
-      return getPackages();
-    }
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(new VectorIconsPackage(), new FBSDKPackage(mCallbackManager));
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+      setActivityCallbacks(new ActivityCallbacks() {
+          @Override
+          public void onActivityResult(int requestCode, int resultCode, Intent data) {
+              mCallbackManager.onActivityResult(requestCode, resultCode, data);
+          }
+      });
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    // If you want to use AppEventsLogger to log events.
+    AppEventsLogger.activateApp(this);
+  }
+
+  @NonNull
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
 }
