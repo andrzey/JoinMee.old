15ae431b230de24df1c1d27e6a81d013e3e38922
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2017-06-09 15:24:04 +0200

    changed how the app on android starts

--- src/app.android.js
+++ src/app.android.js
@@ -7,25 +7,46 @@ import { iconsLoaded } from './utils/app-icons';
 import configureStore from './store/configure-store';
 
 const store = configureStore()
+const state = store.getState();
 
 registerScreens(store, Provider);
 
-Navigation.startSingleScreenApp({
-    screen: {
-        screen: 'example.FirstTabScreen',
-        title: 'App',
-        navigatorStyle: {},
-        topTabs: [
-            {
-                screenId: 'example.FirstTabScreen',
-                title: 'Happenings'
-            },
-            {
-                screenId: 'example.SecondTabScreen',
-                title: 'Mine Happenings'
-            }
-        ]
-    },
-    passProps: {},
-    animationType: 'slide-down'
-});
+export default () => {
+    if (state.user.accessToken) {
+        startTabBasedApp()
+    } else {
+        iconsLoaded.then(() => {
+            Navigation.startSingleScreenApp({
+                screen: {
+                    screen: 'example.Login',
+                    title: 'Login',
+                    navigatorStyle: {},
+                    navigatorButtons: {}
+                }
+            });
+        });
+    }
+}
+
+
+export function startTabBasedApp() {
+    Navigation.startSingleScreenApp({
+        screen: {
+            screen: 'example.FirstTabScreen',
+            title: 'App',
+            navigatorStyle: {},
+            topTabs: [
+                {
+                    screenId: 'example.FirstTabScreen',
+                    title: 'Happenings'
+                },
+                {
+                    screenId: 'example.SecondTabScreen',
+                    title: 'Mine Happenings'
+                }
+            ]
+        },
+        passProps: {},
+        animationType: 'slide-down'
+    });
+}
