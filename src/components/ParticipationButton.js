52cf09fdd013bbfc2beb56c5141a5c12d0b1c813
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2017-06-13 12:06:01 +0200

    changed buttontext

--- src/components/ParticipationButton.js
+++ src/components/ParticipationButton.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { TouchableHighlight, Text, View } from 'react-native';
 
 const ParticipationButton = (props) => {
+    const buttonText = (props.isAttending) ? 'Bli ikke med' : 'Bli med';
     return (
         <TouchableHighlight onPress={props.onPress}>
             <View style={{ width: 100, height: 50, display: 'flex', justifyContent: 'center', backgroundColor: 'lightblue' }}>
                 <Text style={{ textAlign: 'center', fontSize: 14, color: 'white' }}>
-                    {(props.isAttending) ? 'Bli ikke med' : 'Bli med'}
+                    {buttonText}
                 </Text>
             </View>
         </TouchableHighlight>
