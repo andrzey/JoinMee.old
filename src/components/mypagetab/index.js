77c4b5b343ff4bc09dc3c5461c8f392022a724eb
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2017-06-22 15:43:52 +0200

    added loadMyHappenings to mypageTab

--- src/components/mypagetab/index.js
+++ src/components/mypagetab/index.js
@@ -1,16 +1,89 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Text, Platform, View, FlatList, StyleSheet } from 'react-native';
+
+import { iconsMap } from '../../utils/app-icons';
+import * as actions from '../../actions/happening.actions';
+import HappeningListItem from '../happeningtab/HappeningListItem';
 
 class SecondTabScreen extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this._navigateToHappening = this._navigateToHappening.bind(this);
+    this._selectHappening = this._selectHappening.bind(this);
+    this._loadMyHappenings = this._loadMyHappenings.bind(this);
   }
 
   render() {
+    const refreshing = this.props.pendingTasks > 0 ? true : false;
+
     return (
-      <Text>SecondTabScreen!</Text>
+      <View style={styles.container}>
+        <FlatList
+          onRefresh={this._loadMyHappenings}
+          refreshing={refreshing}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={this._renderSeparator}
+          data={this.props.userHappenings}
+          renderItem={({ item }) => <HappeningListItem onPress={this._selectHappening} happening={item} />}
+        />
+      </View>
     );
+  }
+
+  componentDidMount() {
+    this._loadMyHappenings();
+  }
+
+
+  _loadMyHappenings() {
+    this.props.actions.loadMyHappenings(this.props.accessToken);
+  }
+
+  _navigateToHappening(happening) {
+    this.props.navigator.push({
+      screen: 'example.Happening',
+      title: happening.title,
+      animated: true
+    });
+  }
+
+  _renderSeparator = () => {
+    return <View style={styles.listItemSeperator} />
+  }
+
+  _selectHappening(happening) {
+    this.props.actions.setSelectedHappening(happening);
+    this._navigateToHappening(happening);
   }
 }
 
-export default SecondTabScreen
+var styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  listItemSeperator: {
+    height: 1,
+    width: "100%",
+    backgroundColor: "#CED0CE"
+  }
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    userHappenings: state.user.happenings,
+    accessToken: state.user.accessToken,
+    pendingTasks: state.user.pendingTasks,
+    interests: state.user.interests
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SecondTabScreen);
