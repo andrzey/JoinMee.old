55d414b8ddf94143ac115d4e5441c1f570f316d3
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2017-08-25 15:28:37 +0200

    added initialstate to newhappeningreducer

--- src/reducers/new-happening.reducer.js
+++ src/reducers/new-happening.reducer.js
@@ -1,15 +1,20 @@
 import * as actionTypes from '../actions/action-types';
 
-export default function newHappeningReducer(state = {}, action) {
+const initialState = {
+    interests: [],
+    address: null
+}
+
+export default function newHappeningReducer(state = initialState, action) {
     switch (action.type) {
         case actionTypes.ADD_ADDRESS_TO_HAPPENING:
             return Object.assign({}, state, { address: action.address });
         case actionTypes.ADD_INTERESTS_ON_HAPPENING:
             return Object.assign({}, state, { interests: action.interests });
         case actionTypes.ADD_HAPPENING_SUCCEEDED:
-            return {};
+            return initialState;
         case actionTypes.CANCEL_NEW_HAPPENING:
-            return {};
+            return initialState;
         default:
             return state
     }
